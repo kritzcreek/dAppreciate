81348ff3724d60a7d0f5dfb958d19b3338ed0a22
Robin Künzler <robin.kunzler@dfinity.org>
2021-09-07 18:23:31 +0200

    serve assets from index canister

--- index_rs/src/index_rs/src/lib.rs
+++ index_rs/src/index_rs/src/lib.rs
@@ -1,4 +1,7 @@
-use ic_cdk::api::call::{CallResult};
+use certified_map::{AsHashTree, RbTree};
+use hashtree::{Hash};
+use ic_cdk::api::call::CallResult;
+use ic_cdk::api::{set_certified_data, data_certificate};
 use ic_cdk::export::{
     candid::{CandidType, Deserialize},
     Principal,
@@ -6,16 +9,57 @@ use ic_cdk::export::{
 use ic_cdk_macros::*;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
+use std::collections::HashMap;
+use std::borrow::Cow;
+use ic_cdk::trap;
+use serde::Serialize;
+use serde_bytes::{ByteBuf, Bytes};
+use ic_cdk::export::candid::Func;
+
+mod assets;
+
+const LABEL_ASSETS: &[u8] = b"http_assets";
 
 thread_local! {
     static STATE: State = State {
-        donor_to_client_map: RefCell::new(DonorToClientMap::new())
-    }
+        donor_to_client_map: RefCell::new(DonorToClientMap::new()),
+        asset_hashes: RefCell::new(AssetHashes::default()),
+    };
+    static ASSETS: RefCell<HashMap<&'static str, (Vec<HeaderField>, &'static [u8])>> = RefCell::new(HashMap::default());
 }
 
 struct State {
     donor_to_client_map: RefCell<DonorToClientMap>,
+    asset_hashes: RefCell<AssetHashes>,
 }
+
+type HeaderField = (String, String);
+
+type AssetHashes = RbTree<&'static str, Hash>;
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct HttpRequest {
+    method: String,
+    url: String,
+    headers: Vec<(String, String)>,
+    body: ByteBuf,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct HttpResponse {
+    status_code: u16,
+    headers: Vec<HeaderField>,
+    body: Cow<'static, Bytes>,
+    streaming_strategy: Option<StreamingStrategy>,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+enum StreamingStrategy {
+    Callback { callback: Func, token: Token },
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize)]
+struct Token {}
 
 struct DonorToClientMap {
     map: BTreeMap<Donor, Client>,
@@ -164,4 +208,111 @@ async fn donate(receiver: DonationReceiver) {
 #[query]
 fn __get_candid_interface_tmp_hack() -> String {
     include_str!("../index_rs.did").to_string()
+}
+
+#[query]
+fn http_request(req: HttpRequest) -> HttpResponse {
+    let parts: Vec<&str> = req.url.split('?').collect();
+    match parts[0] {
+        probably_an_asset => {
+            let certificate_header = STATE.with(|s| {
+                make_asset_certificate_header(
+                    &s.asset_hashes.borrow(),
+                    probably_an_asset,
+                )
+            });
+
+            ASSETS.with(|a| match a.borrow().get(probably_an_asset) {
+                Some((headers, value)) => {
+                    let mut headers = headers.clone();
+                    headers.push(certificate_header);
+
+                    HttpResponse {
+                        status_code: 200,
+                        headers,
+                        body: Cow::Borrowed(Bytes::new(value)),
+                        streaming_strategy: None,
+                    }
+                }
+                None => HttpResponse {
+                    status_code: 404,
+                    headers: vec![certificate_header],
+                    body: Cow::Owned(ByteBuf::from(format!(
+                        "Asset {} not found.",
+                        probably_an_asset
+                    ))),
+                    streaming_strategy: None,
+                },
+            })
+        }
+    }
+}
+
+fn make_asset_certificate_header(
+    asset_hashes: &AssetHashes,
+    asset_name: &str,
+) -> (String, String) {
+    let certificate = data_certificate().unwrap_or_else(|| {
+        trap("data certificate is only available in query calls");
+    });
+    let witness = asset_hashes.witness(asset_name.as_bytes());
+    let tree = hashtree::labeled(LABEL_ASSETS, witness);
+    let mut serializer = serde_cbor::ser::Serializer::new(vec![]);
+    serializer.self_describe().unwrap();
+    tree.serialize(&mut serializer)
+        .unwrap_or_else(|e| trap(&format!("failed to serialize a hash tree: {}", e)));
+    (
+        "IC-Certificate".to_string(),
+        format!(
+            "certificate=:{}:, tree=:{}:",
+            base64::encode(&certificate),
+            base64::encode(&serializer.into_inner())
+        ),
+    )
+}
+
+#[post_upgrade]
+fn retrieve_data() {
+    init_assets();
+    STATE.with(|s| {
+        update_root_hash(&s.asset_hashes.borrow());
+    });
+}
+
+#[init]
+fn init() {
+    init_assets();
+    STATE.with(|state| {
+        update_root_hash(&state.asset_hashes.borrow());
+    });
+}
+
+// used both in init and post_upgrade
+fn init_assets() {
+    use assets::ContentEncoding;
+
+    STATE.with(|s| {
+        let mut asset_hashes = s.asset_hashes.borrow_mut();
+
+        ASSETS.with(|a| {
+            let mut assets = a.borrow_mut();
+            assets::for_each_asset(|name, encoding, contents, hash| {
+                asset_hashes.insert(name, *hash);
+                let headers = match encoding {
+                    ContentEncoding::Identity => vec![],
+                    ContentEncoding::GZip => {
+                        vec![("Content-Encoding".to_string(), "gzip".to_string())]
+                    }
+                };
+                assets.insert(name, (headers, contents));
+            });
+        });
+    });
+}
+
+fn update_root_hash(a: &AssetHashes) {
+    use hashtree::labeled_hash;
+
+    let prefixed_root_hash = labeled_hash(LABEL_ASSETS, &a.root_hash());
+    set_certified_data(&prefixed_root_hash[..]);
 }
