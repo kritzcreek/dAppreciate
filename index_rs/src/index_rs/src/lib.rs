cfffbe4f3db2a238e2cb645360dfd4d429da7765
Robin Künzler <robin.kunzler@dfinity.org>
2021-09-07 12:01:28 +0200

    implement and test register_client

--- index_rs/src/index_rs/src/lib.rs
+++ index_rs/src/index_rs/src/lib.rs
@@ -34,6 +34,10 @@ impl DonorToClientMap {
     pub fn insert(&mut self, donor: Donor, client: Client) {
         self.map.insert(donor, client);
     }
+
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
 }
 
 #[derive(Clone, Debug, CandidType, Deserialize)]
@@ -62,8 +66,21 @@ fn print() {
 }
 
 #[update]
-fn register_client(client: Client) {
-    ic_cdk::print(format!("Called register_client for {:?}", client));
+fn register_client(client: Client) -> u128 {
+    let donor = Donor {
+        donor: ic_cdk::caller(),
+    };
+    ic_cdk::print(format!(
+        "Registering client {:?} for donor {:?}",
+        client, donor
+    ));
+    let mut map_len = 0;
+    STATE.with(|s| {
+        let mut donor_to_client_map = s.donor_to_client_map.borrow_mut();
+        donor_to_client_map.insert(donor, client);
+        map_len = donor_to_client_map.len();
+    });
+    map_len as u128
 }
 
 #[update]
@@ -80,4 +97,15 @@ fn current_client() -> Option<Client> {
 #[update]
 fn donate(receiver: DonationReceiver) {
     ic_cdk::print(format!("Called donate for {:?}", receiver));
+}
+
+/// This makes this Candid service self-describing, so that for example Candid UI, but also other
+/// tools, can seamlessly integrate with it. The concrete interface (method name etc.) is
+/// provisional, but works.
+///
+/// This is needed by the ic-repl tests, see
+/// https://github.com/chenyan2002/ic-repl#notes-for-rust-canisters
+#[query]
+fn __get_candid_interface_tmp_hack() -> String {
+    include_str!("../index_rs.did").to_string()
 }
---
+++ index_rs/src/index_rs/tests/tests.sh
@@ -0,0 +1,13 @@
+// To run these tests locally:
+// * add ic-repl to you PATH, it can be downloaded from here: https://github.com/chenyan2002/ic-repl/releases
+// * deploy the canisters needed for this tests e.g. using ./deploy.sh
+// * then run % ic-repl tests.sh
+
+#!ic-repl -r http://localhost:8000
+
+import IndexCanister = "q3fc5-haaaa-aaaaa-aaahq-cai";
+
+// should successfully register a client
+call IndexCanister.register_client(record
+          { client_canister_id = principal "fjm4k-j73tc-uhkc4-3hhrk-qi4hk-swun5-4xxzd-enw73-p4sm7-q2or5-nqe" });
+assert _ ~= (1 : nat);
