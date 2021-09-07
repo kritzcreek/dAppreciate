d7d910611afb5ff2c282b0398fb5d86cee165836
Franz-Stefan Preiss <franzstefan@dfinity.org>
2021-09-07 12:45:35 +0200

    Stub implementation of donate

--- index_rs/src/index_rs/src/lib.rs
+++ index_rs/src/index_rs/src/lib.rs
@@ -57,7 +57,8 @@ struct ApprovedClient {
 
 #[derive(Clone, Debug, CandidType, Deserialize)]
 struct DonationReceiver {
-    pub donation_receiver_canister_id: Principal,
+    pub receiver: Principal,
+    pub beneficiaries: Vec<Principal>,
 }
 
 #[query]
@@ -93,9 +94,28 @@ fn current_client() -> Option<Client> {
     None
 }
 
+// Issues an inter canister call to the `Client` that is registered for the
+// self-authenticating principal issuing this call. The mapping to the client
+// must be active.
+//
+// Authentication: the call must be made using a self-authenticating principal
+//
+// Traps if the caller is anonymous.
 #[update]
 fn donate(receiver: DonationReceiver) {
     ic_cdk::print(format!("Called donate for {:?}", receiver));
+    trap_if_caller_anonymous();
+    let donor_principal = Donor {
+        donor: ic_cdk::caller(),
+    };
+    let donor_client = STATE.with(|s| s.donor_to_client_map.borrow().get(&donor_principal));
+    if let Some(donor) = donor_client {
+        let future = ic_cdk::call(donor.client_canister_id, "donate", (receiver,));
+        let r = ic_cdk::block_on(future);
+    } else {
+        // TODO: change this to an error so that we can handle this case in the UI
+        ic_cdk::trap(&format!("Found no mapping for this donor."))
+    }
 }
 
 /// This makes this Candid service self-describing, so that for example Candid UI, but also other
@@ -107,4 +127,11 @@ fn donate(receiver: DonationReceiver) {
 #[query]
 fn __get_candid_interface_tmp_hack() -> String {
     include_str!("../index_rs.did").to_string()
+}
+
+// Traps if the caller is anonymous.
+fn trap_if_caller_anonymous() {
+    if ic_cdk::api::caller() == Principal::anonymous() {
+        ic_cdk::trap(&format!("Caller must not be anonymous."))
+    }
 }
