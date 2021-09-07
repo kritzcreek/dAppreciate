c5e4aae05521b3bdf6c733c99e776ce1579536df
Robin Künzler <60650552+robin-kunzler@users.noreply.github.com>
2021-09-07 10:19:18 +0200

    introduce index canister storage (#2)

--- index_rs/src/index_rs/src/lib.rs
+++ index_rs/src/index_rs/src/lib.rs
@@ -1,9 +1,49 @@
-use ic_cdk::export::{candid::{CandidType, Deserialize}, Principal};
+use ic_cdk::export::{
+    candid::{CandidType, Deserialize},
+    Principal,
+};
 use ic_cdk_macros::*;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+
+thread_local! {
+    static STATE: State = State {
+        donor_to_client_map: RefCell::new(DonorToClientMap::new())
+    }
+}
+
+struct State {
+    donor_to_client_map: RefCell<DonorToClientMap>,
+}
+
+struct DonorToClientMap {
+    map: BTreeMap<Donor, Client>,
+}
+
+impl DonorToClientMap {
+    pub fn new() -> Self {
+        Self {
+            map: BTreeMap::new(),
+        }
+    }
+
+    pub fn get(&self, donor: &Donor) -> Option<&Client> {
+        self.map.get(&donor)
+    }
+
+    pub fn insert(&mut self, donor: Donor, client: Client) {
+        self.map.insert(donor, client);
+    }
+}
 
 #[derive(Clone, Debug, CandidType, Deserialize)]
 struct Client {
     pub client_canister_id: Principal,
+}
+
+#[derive(Clone, Debug, PartialOrd, Ord, Eq, PartialEq, Deserialize)]
+struct Donor {
+    pub donor: Principal,
 }
 
 #[derive(Clone, Debug, CandidType, Deserialize)]
